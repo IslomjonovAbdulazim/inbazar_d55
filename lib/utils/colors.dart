bfd617b597409a7ec593d8a3203fe26bfb403e99
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-24 18:11:46 +0500

    imp

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,1 +1,12 @@
-class AppColors {}
+import 'dart:ui';
+
+class AppColors {
+  static const Color inbazarGreen = Color(0xFF00A541);
+  static const Color jetBlack = Color(0xFF222222);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color cloudGray = Color(0xFFF5F5F5);
+  static const Color charcoalGray = Color(0xFF3D3D3D);
+  static const Color accentYellow = Color(0xFFFFD700);
+  static const Color errorRed = Color(0xFFE53935);
+  static const Color skyBlue = Color(0xFF00BCD4);
+}
