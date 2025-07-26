21df3b59d6d604bf5d5850310548ab052edd28dc
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-26 17:42:23 +0500

    imp

--- lib/pages/auth/body_info_page.dart
+++ lib/pages/auth/body_info_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets.dart';
+
 class BodyInfoPage extends StatefulWidget {
   const BodyInfoPage({super.key});
 
@@ -8,8 +10,29 @@ class BodyInfoPage extends StatefulWidget {
 }
 
 class _BodyInfoPageState extends State<BodyInfoPage> {
+  List<String> steps = [
+    "Jins", // 1-erkak, 2-ayol
+    "Yosh",
+    "Bo'y",
+    "Vazn",
+    "Oyoq Kiyim",
+    "Kiyim Stili", // tor(1), o'rta(2), keng(3)
+    "Tana Tuzilishi", // ozgin(1), o'rta(2), semiz(3), sportchi(4)
+  ];
+  List<int> values = [0, 18, 180, 70, 42, 2, 2];
+
+  List<Widget> widgets = [
+    GenderSelect(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: GenderSelect()),
+        ],
+      ),
+    );
   }
 }
