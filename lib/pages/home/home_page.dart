9b6bb779344c5dc138df89ad8e01499f48602fb2
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-31 17:40:06 +0500

    imp

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:inbazar_d55555/models/mock_data.dart';
 import 'package:inbazar_d55555/models/product_model.dart';
 import 'package:inbazar_d55555/pages/home/profile_page.dart';
 import 'package:inbazar_d55555/pages/home/widgets/product_widget.dart';
+import 'package:inbazar_d55555/utils/texts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,7 +18,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         actions: [
           CupertinoButton(
             onPressed: () {
@@ -31,21 +34,47 @@ class _HomePageState extends State<HomePage> {
           SizedBox(width: 8),
         ],
       ),
-      body: ListView(
-        children: [
-          GridView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          children: [
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                itemCount: mockCategories.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  final model = CategoryModel.fromJson(mockCategories[index]);
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: CupertinoButton(
+                      color: Colors.grey.shade100,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      onPressed: () {},
+                      child: Text(
+                        model.title,
+                        style: TextStyles.title,
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-            itemCount: mockProducts.length,
-            itemBuilder: (context, index) {
-              final model = ProductModel.fromJson(mockProducts[index]);
-              return ProductWidget(model: model);
-            },
-          ),
-        ],
+            SizedBox(height: 20),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: mockProducts.length,
+              itemBuilder: (context, index) {
+                final model = ProductModel.fromJson(mockProducts[index]);
+                return ProductWidget(model: model);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
