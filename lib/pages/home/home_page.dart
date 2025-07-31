54134809198efd6f78dd2a46f7f7a44f119e3ea9
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-31 17:23:55 +0500

    imp

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -38,3 +38,5 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
---
+++ lib/pages/home/widgets/product_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:inbazar_d55555/models/product_model.dart';
+
+class ProductWidget extends StatelessWidget {
+  final ProductModel model;
+  const ProductWidget({super.key, required this.model});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
